942dc92851ba672efc6855d795169a50fed49064
Mario Avila <avilamario16@gmail.com>
2019-12-16 12:05:21 -0600

    develop the beer section

--- src/components/TheBeer.vue
+++ src/components/TheBeer.vue
@@ -1,29 +1,57 @@
 <template>
   <div class="the-beer-container" ref="TheBeer" id="TheBeer">
-    <RibbonTitle 
-      title="la cerveza" 
-      subtitle="una lager con un gran sabor en botella transparente"></RibbonTitle>
-    <div class="content">
-      <div class="the-beer_image-title">
-        <!-- <img src="" alt=""> -->
-        <h1>the champange of beers</h1>
+    <RibbonTitle title="la cerveza" subtitle="una lager con un gran sabor en botella transparente"></RibbonTitle>
+    <div class="thebeer_content">
+      <div class="thebeer_text-content">
+        <div class="thebeer_logo-row">
+          <div class="thebeer_imagelogo-container">
+            <img :src="millerhighlifelogoImage" alt class="thebeer_imagelogo" />
+          </div>
+          <div class="thebeer_phrase-container">
+            <img :src="millerhighlifephraseImage" alt class="thebeer_phrase" />
+          </div>
+        </div>
         <p>
-          La favorita entre los bartenders, cerveceros, y amantes de la cerveza 
-          en general. Miller High Life fue lanzada como la marca principal de 
-          Miller Brewing Company en 1903. Su sabor fácil de beber es el resumen 
-          de la categoría de las American lagers. Fiel a su receta original, 
-          se elabora con una mezcla de cebada y malta patentada. 
+          La favorita entre los bartenders, cerveceros, y amantes de la cerveza
+          en general. Miller High Life fue lanzada como la marca principal de
+          Miller Brewing Company en 1903. Su sabor fácil de beber es el resumen
+          de la categoría de las American lagers. Fiel a su receta original,
+          se elabora con una mezcla de cebada y malta patentada.
           Lúpulo Galena del noroeste del Pacífico y levadura Miller.
         </p>
+        <div class="table-values">
+          <table>
+            <tbody>
+              <tr>
+                <th class="abv-val">ABV</th>
+                <th class="ibu-val">IBU</th>
+                <th class="cal-val">Calorías</th>
+              </tr>
+              <tr>
+                <td class="abv-num">4.6%</td>
+                <td class="ibu-num">7</td>
+                <td class="cal-num">141</td>
+              </tr>
+              <tr></tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div class="the-beer_image-title">
+        <img :src="millerhighlifebottleImage" alt />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import millerhighlifelogo from "@/assets/miller-high-life-centered-logo.jpeg";
+import millerhighlifephrase from "@/assets/miller-high-logo-phrase.png";
+import millerhighlifebottle from "@/assets/MHL_Bottles_Single.png";
+
 import { mapActions, mapGetters } from "vuex";
 
-import RibbonTitle from '@/components/RibbonTitle';
+import RibbonTitle from "@/components/RibbonTitle";
 
 export default {
   name: "TheBeer",
@@ -34,7 +62,11 @@ export default {
     ...mapGetters([""])
   },
   data() {
-    return {};
+    return {
+      millerhighlifelogoImage: millerhighlifelogo,
+      millerhighlifephraseImage: millerhighlifephrase,
+      millerhighlifebottleImage: millerhighlifebottle
+    };
   },
   methods: {
     ...mapActions({})
@@ -43,5 +75,5 @@ export default {
 </script>
 
 <style scoped lang="scss">
-// @import "~@/styles/thebeer.scss";
+@import "~@/styles/thebeer.scss";
 </style>
