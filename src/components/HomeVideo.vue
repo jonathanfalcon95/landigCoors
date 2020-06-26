af9cb1a1a2fa51caebfec19608a396e40e0c61b3
Jonathanfalcon95 <jonathan.falcon.95@hotmail.com>
2020-06-26 03:07:44 -0400

    add video youtube

--- src/components/HomeVideo.vue
+++ src/components/HomeVideo.vue
@@ -5,31 +5,40 @@
      
 
       <div class="workhorse-arrow modal-open" data-backdrop="false" data-target="#modal-container-qYeF5_KPM_c">
-    <img src="https://www.coorslight.com/sites/CoorsLightRD/files/icon-play.png" width="160" height="160" alt="Play" title="Play"></div>
+    <img @click="popUpModal()" src="https://www.coorslight.com/sites/CoorsLightRD/files/icon-play.png" width="160" height="160" alt="Play" title="Play">
+    </div>
+     <ModalVideo v-if="showModal" v-bind:wichModal="wichModal" @close="showModal = false" />
     </div>
   </div>
 </template>
 
 <script>
 import coorslogo from "@/assets/1x.jpg";
-
+import ModalVideo from "@/components/ModalVideo";
 import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "homeVideo",
   components: {
-    coorslogoImage: coorslogo
+    coorslogoImage: coorslogo,
+    ModalVideo
   },
   computed: {
     ...mapGetters([""])
   },
   data() {
     return {
-      coorslogoImage: coorslogo
+      coorslogoImage: coorslogo,
+      showModal: false,
     };
   },
   methods: {
-    ...mapActions({})
+    ...mapActions({}),
+
+     popUpModal() {
+      this.showModal = true;
+     // this.wichModal = modalType;
+    },
   }
 };
 </script>
--- src/components/ModalVideo.vue
+++ src/components/ModalVideo.vue
@@ -14,7 +14,24 @@
           <div class="modal-body-body">
             <slot name="body">
               <Terms v-if="wichModal === 'terms'" name="millerlite" />
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/AnGnIBlJgbk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <iframe width="100%" height="100%" src="https://www.youtube.com/embed/AnGnIBlJgbk?enablejsapi=1&amp;autoplay=1" frameborder="0"  allowfullscreen="1" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+           <!-- <iframe id="video-container-qYeF5_KPM_c" data-videoid="qYeF5_KPM_c" class="modal-content ytplayer" frameborder="0" allowfullscreen="1" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" title="YouTube video player" width="100%" height="100%" src="https://www.youtube.com/embed/qYeF5_KPM_c?enablejsapi=1&amp;origin=https%3A%2F%2Fwww.coorslight.com&amp;widgetid=1&amp;mute=1&amp;autoplay=1"></iframe> -->
+           <!-- -webkit-tap-highlight-color: rgba(0,0,0,0);
+font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+font-size: 14px;
+line-height: 1.42857143;
+color: #ffffff;
+width: 100%;
+height: 100%;
+box-sizing: border-box;
+position: relative;
+background-color: #fff;
+background-clip: padding-box;
+border: 1px solid rgba(0,0,0,.2);
+border-radius: 6px;
+outline: 0;
+box-shadow: 0 5px 15px rgba(0,0,0,.5);
+border-top: none; -->
             </slot>
           </div>
 
